5171f767c39065f1a0f8319b0680ed5d639a52c8
Balázs Csathó <csatho.balazs@gmail.com>
2023-02-28 13:32:16 +0100

    map visualizations

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
+import hvplot.pandas
 import pandas as pd
-import datetime as dt
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
 import numpy as np
 import panel as pn
-pn.extension()
+import datetime as dt
+import folium
+import plotly.express as px
+import plotly.graph_objects as go
+pn.extension('plotly')
 
-path = 'res\WHO-COVID-19-global-data.csv'
+
 url = 'https://covid19.who.int/WHO-COVID-19-global-data.csv'
 data = pd.read_csv(url)
+idata = data.interactive()
+data['Date_reported'] = pd.to_datetime(data['Date_reported']).dt.date
 
+date_slider = pn.widgets.DateSlider(
+    name='Date', start=dt.date(2020, 1, 3), end=dt.date.today())
+column = pn.widgets.Select(name='Column', options=[
+                           'New_cases', 'Cumulative_cases', 'New_deaths', 'Cumulative_deaths'])
 
-def plot_by_country(variable='Hungary'):
-    df = data.loc[data['Country'] == variable]
-    date = df["Date_reported"]
-    cum_cases = df["Cumulative_cases"]
+df = data.loc[data["Date_reported"] == dt.date(2023, 1, 1)]
 
-    date = np.asarray(date, dtype='datetime64[s]')
-    return [date, cum_cases]
-
-
-countries = data.loc[data['Country']].tolist()
-country = pn.widgets.Select(name='Country', options=countries)
-
-pn.bind(plot_by_country, country=country)
-
-
-interactive = pn.bind(plot_by_country, country)
-
-first_app = pn.Column(country, interactive)
+cov_pipeline = (
+    idata[
+        (idata.Date_reported == date_slider)
+    ]
+    .loc[:, ['Date_reported', 'Country']]
+)
+choropleth = go.Figure(data=go.Choropleth(
+    locations=data["Country"],
+    z=data["Cumulative_cases"],
+    locationmode="country names"
+))
+layout = go.Layout(
+    title='Scatterplot of Cumulative cases',
+    width=1500,
+    height=700
+)
+cov_plot = cov_pipeline.hvplot(choropleth)
+cov_plot
